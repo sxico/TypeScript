14dbbe83ed4b9aa63e51fce792ff7bc14cc546f5
sxico <francisco.asevero@gmail.com>
2021-07-16 14:37:48 -0300

    Add metodos opcionais para expReg de template

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -9,7 +9,7 @@ export class NegociacaoController{
     private inputQuantidade: HTMLInputElement;
     private inputValor: HTMLInputElement;
     private negociacoes = new Negociacoes;
-    private negociacoesView = new NegociacoesView('#negociacoesView');
+    private negociacoesView = new NegociacoesView('#negociacoesView', true);
     private mensagemView = new MensagemView('#mensagemView');
 
     constructor(){
@@ -20,7 +20,12 @@ export class NegociacaoController{
     }
 
     public adiciona():void {
-        const negociacao = this.criaNegociacao();
+        const negociacaoTemp = new Negociacao(null, 0, 0);
+        const negociacao = Negociacao.criaDe(
+            this.inputData.value,
+            this.inputQuantidade.value,
+            this.inputValor.value
+        );
         if(!this.diaUtil(negociacao.data)){
             this.mensagemView.update('Somente dias úteis são aceitos para a negociação!');
             return ;
@@ -35,13 +40,8 @@ export class NegociacaoController{
                 && data.getDay() < DiasDaSemana.SABADO)
     }
 
-    private criaNegociacao():Negociacao {
-        const exp = /-/g;
-        const date = new Date(this.inputData.value.replace(exp, ','));
-        const quantidade = parseInt(this.inputQuantidade.value);
-        const valor = parseFloat(this.inputValor.value);
-        return new Negociacao(date, quantidade, valor);
-    }
+    // private criaNegociacao():Negociacao {
+    // }
 
     private limparFormulario(): void {
         this.inputData.value = '';
