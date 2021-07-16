1cc65c6867e318930f5ad853c88233b9b8c4710b
sxico <francisco.asevero@gmail.com>
2021-07-16 13:06:55 -0300

    Explicitando tipo de metodos (private; public; protected e organizando as classes

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -3,7 +3,7 @@ import { View } from "./view.js";
 
 export class NegociacoesView extends View<Negociacoes>{
 
-    template(model: Negociacoes): string {
+    protected template(model: Negociacoes): string {
         return`
             <table class="table table-hover table-bordered">
                 <thead>
@@ -17,8 +17,7 @@ export class NegociacoesView extends View<Negociacoes>{
                 ${model.lista().map(negociacao => {
                     return `
                         <tr>
-                            <td>${new Intl.DateTimeFormat()
-                                .format(negociacao.data)}</td>
+                            <td>${this.formatarData(negociacao.data)}</td>
                             <td>${negociacao.quantidade}</td>
                             <td>${negociacao.valor}</td>
                         </tr>
@@ -27,5 +26,10 @@ export class NegociacoesView extends View<Negociacoes>{
                 </tbody>
             </table>       
         `;
+    }
+
+    private formatarData(data: Date): string {
+        return new Intl.DateTimeFormat()
+            .format(data);
     }
 }
