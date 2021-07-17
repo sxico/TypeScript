489b8d33cd08d017c83ebb63ec88d784700643f7
sxico <francisco.asevero@gmail.com>
2021-07-17 16:11:54 -0300

    Add uso de decorators para medição de performance dos métodos

--- app/views/view.ts
+++ app/views/view.ts
@@ -3,7 +3,12 @@ export abstract class View<T> {
     private escapar: boolean = false;
 
     constructor(seletor: string, escapar?: boolean){
-        this.elemento = document.querySelector(seletor);
+        const elemento = document.querySelector(seletor);
+        if(elemento){
+            this.elemento = elemento as HTMLElement;
+        } else{
+            throw Error(`Seletor ${seletor} não existe no DOM!`);
+        }
         if(escapar){
             this.escapar = escapar;
         }
